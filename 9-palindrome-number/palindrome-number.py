189c166f0485eb41c82499a31b67d0e8f07f3a18
AkubaruF <112474451+AkubaruF@users.noreply.github.com>
2026-02-14 20:57:55 +0700

    Time: 4 ms (90.39%) | Memory: 12.5 MB (55.97%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -4,14 +4,5 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        if x < 0:
-            return False
-
-        string = str(x)
-        for i in range(len(string)):
-            if string[i] == string[len(string) - i - 1]:
-                continue
-            else:
-                return False
-        return True
+        return str(x) == str(x)[::-1]
         
